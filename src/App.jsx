ef143cf2ad5737f6a0bbc7b4614925f3b1451fc9
Faisal Khan <faisaldev404@gmail.com>
2023-09-07 17:07:37 +0500

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import MovieList from "./components/MovieList/MovieList";
 import Navbar from "./components/navbar/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
       <MovieList types="popular" title="Popular" />
       <MovieList types="top_rated" title="Top Rated" />
       <MovieList types="upcoming" title="UpComing" />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      This is developed by
+      <a href="https://faisalkhandev.com/" target="_blank" rel="noreferrer">
+        <span className="footerName"> Faisal Khan</span>
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
